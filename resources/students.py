322f102859ea676aa0740b816309ef6dd76147dd
ZhePang <zhepang@pitzer.students.edu>
2023-09-20 20:31:05 -0400

    changed file path

--- resources/students.py
+++ resources/students.py
@@ -8,7 +8,7 @@ class StudentsResource:
     #
 
     students_file = \
-        "/Users/donaldferguson/Dropbox/000-Examples/e6156-microservice-1/resources/old-students.json"
+        "/Users/danielzzzz/Documents/Columbia/Fall 2023/Cloud Computing/e6156-microservice-1/resources/old-students.json"
 
     def __init__(self):
         self.students = None
